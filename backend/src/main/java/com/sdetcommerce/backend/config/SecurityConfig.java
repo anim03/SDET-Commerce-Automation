76e6a2fd5b5171e0cafdb17b60612f551d2b9f69
Animesh Pandey <animo@Animo.local>
2026-09-12 09:46:32 +0530

    feat: prepare application for cloud deployment

--- backend/src/main/java/com/sdetcommerce/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sdetcommerce/backend/config/SecurityConfig.java
@@ -201,10 +201,13 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
+        String frontendUrl = System.getenv().getOrDefault(
+                "FRONTEND_URL",
+                "http://localhost:5173"
+        );
+
         configuration.setAllowedOrigins(
-                List.of(
-                        "http://localhost:5173"
-                )
+                List.of(frontendUrl)
         );
 
         configuration.setAllowedMethods(
